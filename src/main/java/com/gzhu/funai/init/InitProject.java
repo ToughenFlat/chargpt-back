f500d6d36705933017aadc56de985918a074048e
ToughenFlat <toughenflat@gmail.com>
2023-07-25 18:47:52 +0800

    ChatGPT项目启动测试AI访问

---
+++ src/main/java/com/gzhu/funai/init/InitProject.java
@@ -0,0 +1,26 @@
+package com.gzhu.funai.init;
+
+import com.gzhu.funai.api.openai.ChatGPTApi;
+import com.gzhu.funai.api.openai.enums.Role;
+import com.gzhu.funai.api.openai.req.ChatGPTReq;
+import com.gzhu.funai.api.openai.req.ContextMessage;
+import com.gzhu.funai.api.openai.resp.ChatGPTResp;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class InitProject implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ChatGPTResp resp = ChatGPTApi.sessionReq(
+                ChatGPTReq.builder().messages(Arrays.asList(new ContextMessage(Role.USER.name, "请问如何评价秦始皇?"))).build(),
+                "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");
+
+        System.out.println(resp.getChoices().get(0).getMessage().toString());
+        System.out.println("初始化项目");
+    }
+}
