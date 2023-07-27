7540d9b3813958c12712a013bdbc9f97f313f9e5
ToughenFlat <toughenflat@gmail.com>
2023-07-27 09:51:38 +0800

    ChatGPT项目启动测试AI访问优化5

--- src/main/java/com/gzhu/funai/init/InitProject.java
+++ src/main/java/com/gzhu/funai/init/InitProject.java
@@ -5,8 +5,10 @@ import com.gzhu.funai.api.openai.enums.Role;
 import com.gzhu.funai.api.openai.req.ChatGPTReq;
 import com.gzhu.funai.api.openai.req.ContextMessage;
 import com.gzhu.funai.api.openai.resp.ChatGPTResp;
+import com.gzhu.funai.entity.UserApiKeyEntity;
 import com.gzhu.funai.enums.ApiType;
 import com.gzhu.funai.service.AdminApiKeyService;
+import com.gzhu.funai.service.UserApiKeyService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -17,13 +19,14 @@ import java.util.Arrays;
 @Component
 public class InitProject implements ApplicationRunner {
     @Resource
-    private AdminApiKeyService adminApiKeyService;
+    private UserApiKeyService userApiKeyService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        UserApiKeyEntity userApiKeyEntity = userApiKeyService.getByUserIdAndType("1", ApiType.OPENAI);
         ChatGPTResp resp = ChatGPTApi.sessionReq(
                 ChatGPTReq.builder().messages(Arrays.asList(new ContextMessage(Role.USER.name, "请问如何评价秦始皇?"))).build(),
-                adminApiKeyService.roundRobinGetByType(ApiType.OPENAI));
+                userApiKeyEntity.getApikey());
         if (resp != null) {
             System.out.println(resp.getChoices().get(0).getMessage().toString());
         }
